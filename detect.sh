96f4fa8904ff0f0694eba3f86b3660b6434e10ec
Enrico Razzetti <sub@krutt.org>
2017-08-29 20:27:12 +0200

    added detect ip script

---
+++ detect.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+## ip detect script for eth0
+
+set -o nounset -o errexit
+export PATH=/usr/sbin:/usr/bin:$PATH
+echo $(ip addr show eth0 | grep -Eo '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | head -1)
+
